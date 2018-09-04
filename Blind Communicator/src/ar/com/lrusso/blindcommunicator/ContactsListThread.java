830a02d10e192274d50d01af75cda4566e14414f
Leonardo Javier Russo <lrusso@Mac-Mini.local>
2018-09-04 20:54:21 -0300

    -
    -

--- Blind Communicator/src/ar/com/lrusso/blindcommunicator/ContactsListThread.java
+++ Blind Communicator/src/ar/com/lrusso/blindcommunicator/ContactsListThread.java
@@ -33,8 +33,8 @@ public class ContactsListThread extends AsyncTask<Context, String, Boolean>
 				{
 				try
 					{
-					String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-					String phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+					String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)).trim();
+					String phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)).trim();
 					GlobalVars.contactDataBase.add(name.concat("|").concat(phoneNumber));
 					}
 					catch(NullPointerException e)
@@ -44,7 +44,47 @@ public class ContactsListThread extends AsyncTask<Context, String, Boolean>
 					{
 					}
 				}
+
+		    // SORTING CONTACTS
 			Collections.sort(GlobalVars.contactDataBase,new Comparator<String>(){public int compare(String s1, String s2){return s1.compareToIgnoreCase(s2);}});
+
+		    // DELETING REPEATED VALUES
+			try
+				{
+				for (int i=0;i<=GlobalVars.contactDataBase.size();i++)
+					{
+					try
+						{
+						String contact1 = GlobalVars.contactDataBase.get(i);
+						String contact1Name = GlobalVars.getContactNameFromPhoneNumber(contact1);
+						String contact1Phone = GlobalVars.contactsGetPhoneNumberFromListValue(contact1);
+						
+						for (int a=0;a<=GlobalVars.contactDataBase.size();a++)
+							{
+							try
+								{
+								String contact2 = GlobalVars.contactDataBase.get(a);
+								String contact2Name = GlobalVars.getContactNameFromPhoneNumber(contact2);
+								String contact2Phone = GlobalVars.contactsGetPhoneNumberFromListValue(contact2);
+								
+								if (contact1Name.equals(contact2Name) && contact1Phone.equals(contact2Phone) && i!=a)
+									{
+						        	GlobalVars.contactDataBase.remove(a);
+									}
+								}
+								catch(Exception e)
+								{
+								}
+							}
+						}
+						catch(Exception e)
+						{
+						}
+					}
+				}
+				catch(Exception e)
+				{
+				}
 			}
 			catch(NullPointerException e)
 			{
@@ -74,4 +114,18 @@ public class ContactsListThread extends AsyncTask<Context, String, Boolean>
 			{
 			}
 		}
+	
+	private void removeDuplicates(List<String> list)
+		{
+	    final Set<String> encountered = new HashSet<String>();
+	    for (Iterator<String> iter = list.iterator(); iter.hasNext(); )
+	    	{
+	        final String t = iter.next();
+	        final boolean first = encountered.add(t);
+	        if (!first)
+	        	{
+	            iter.remove();
+	        	}
+	    	}
+		}	
 	}
